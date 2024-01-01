8679545f8545a551513e1a2fd50f3f77d87c0082
rocklm <lrock1260@gmail.com>
2024-01-01 21:37:56 +0000

    removed mp3 from title and moving feat_word_changes function to main call also edited the feat word funtion to change the file path instead of the import

--- mp3_formatter.py
+++ mp3_formatter.py
@@ -17,6 +17,7 @@ from logging import getLogger
 import os
 import eyed3
 import re
+import time
 
 #supress CRC error from eyed3
 getLogger().setLevel('ERROR')
@@ -24,37 +25,49 @@ getLogger().setLevel('ERROR')
 #set music foled location
 music_folder = 'C:\Entertainment\Music'
 
-files = os.listdir(music_folder)
+#song names
+
 
 #'featuring' has a consistent naming convention
-def feat_word_change(song_string):
+def feat_word_change():
     
-    patterns = ['Ft.','ft.', 'Feat.', 'feat.', 'ft ', 'Ft', 'feat']
-    for pattern in patterns:
-        return song_string.replace(pattern, 'Feat')
+    patterns = ['Featuring', 'featuring', 'Ft ', 'ft ', 'FT', 'feat']
+    files = os.listdir(music_folder)
+
+    for song_name in files:
+        old_song_path = music_folder + '\\' + song_name
+    
+        for pattern in patterns:
+            new_song_path = old_song_path.replace(pattern, 'Feat.')
+        
+        os.rename(old_song_path, new_song_path)
+            
 
 #split title from artist and assign each to relevant field
 def format_song(song_string):
-
-    fix_feat = feat_word_change(song_string)
-
-    split_string = fix_feat.split('-')
-    if len(split_string) < 1:
+    
+    split_string = song_string.split('-')
+    if len(split_string) < 2:
+        #song.tag.title = song_string.replace('.mp3', '')
+        #song.tag.save() 
         pass
     else:
         song = eyed3.load(music_folder + '\\' + song_string)
         song.tag.album_artist = split_string[0]
-        song.tag.title = split_string[1]
+        song.tag.title = split_string[1].replace('.mp3', '')
         song.tag.save() 
 
 #clean each song in file
 def clean_songs():
-    for song in files:
-        format_song(song)
+    files = os.listdir(music_folder)
+    for song_name in files:
+        format_song(song_name)
 
 #call clean
 if __name__ == '__main__':
+    feat_word_change() 
     clean_songs()
+    
 
 
 
